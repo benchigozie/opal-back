622bd5d77bd307908ed47552d6d033a5de21ae76
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2025-06-17 01:37:28 +0100

    feat: add login funtion in auth controller.

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -78,8 +78,52 @@ const registerUser = async (req, res) => {
   }
 };
 
-const loginUser = (req, res) => {
-  console.log("Logging in user:", req.body.email);
+const loginUser = async (req, res) => {
+
+  const { email, password } = req.body;
+  
+  try {
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      res.status(404).json({ message: "User not found"})
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const accessToken = generateAccessToken({ id: user.id, role: user.role });
+    const refreshToken = generateRefreshToken({ id: user.id, role: user.role });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'Strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    return res.status(200).json({
+      message: 'Login successful',
+      accessToken,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+    
+  } catch (error) {
+
+    console.error('Login error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
   res.status(200).json({ message: "User logged in (placeholder)" });
 };
 module.exports = { registerUser, loginUser };
